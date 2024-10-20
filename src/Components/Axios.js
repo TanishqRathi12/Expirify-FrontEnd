336e78954bfb5d281785f9a4454bfb83c0da75c9
TanishqRathi12 <rathitanishq2022@gmail.com>
2024-10-20 15:27:57 +0530

    link updatef

--- src/Components/Axios.js
+++ src/Components/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://192.168.24.190:3000/', 
+    baseURL: 'https://expirify-backend.onrender.com/', 
     headers: {
         'Content-Type': 'application/json',
     },
